7c056eaa1ac71c474db650091274a5723e21794a
juansotelo1709 <juan.david.jds84@gmail.com>
2025-07-12 19:48:19 -0500

    agrega codigo_mejorado

---
+++ onix/bitmaps.h
@@ -0,0 +1,22 @@
+#ifndef BITMAPS_H
+#define BITMAPS_H
+
+#include <Arduino.h>
+
+extern const unsigned char cat_openojosabiertos[] PROGMEM;
+extern const unsigned char cat_openojoscerrados[] PROGMEM;
+extern const unsigned char epd_bitmap_bocaAbierta[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua1[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua2[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua3[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua4[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua5[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua6[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua7[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua8[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua9[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua10[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua11[] PROGMEM;
+extern const unsigned char epd_bitmap_lengua12[] PROGMEM;
+
+#endif // BITMAPS_H
