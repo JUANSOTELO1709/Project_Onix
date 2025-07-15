704f261e71d21999a51d4a980c8fc99a4293d08d
juansotelo1709 <juan.david.jds84@gmail.com>
2025-07-14 21:45:26 -0500

    agrega codigo_mejorad7

---
+++ onix/epd_bitmap_lengua1_rle.h
@@ -0,0 +1,9 @@
+// RLE-compressed bitmap for 'lengua1', 128x64px
+const uint8_t epd_bitmap_lengua1_rle[] PROGMEM = {
+    // Example RLE data (replace with actual compressed data)
+    0xFF, 0xE0, 0x08, 0x00, 0x07, 0xFF, 0xF8, 0x10, 0x00, 0x07, 0xFF, 0xF0, 0x20, 0x00, 0x07, 0xFF,
+    // ... (rest of RLE data should be generated from the original bitmap)
+};
+
+#define epd_bitmap_lengua1_rle_width 128
+#define epd_bitmap_lengua1_rle_height 64
---
+++ onix/epd_bitmap_lenguaTapaPantalla_rle.h
@@ -0,0 +1,8 @@
+// Bitmap comprimido con RLE para epd_bitmap_lenguaTapaPantalla
+const unsigned char epd_bitmap_lenguaTapaPantalla_rle[] PROGMEM = {
+  0xff,1, 0xe0,1, 0x00,6, 0x07,1, 0xff,1,
+  0xff,16,
+  0xff,16,
+  0xff,4, 0xe0,1, 0x00,1, 0xff,1, 0xf0,1, 0x00,1, 0x1f,1, 0xff,4,
+  // ...continúa el resto del bitmap comprimido...
+};
